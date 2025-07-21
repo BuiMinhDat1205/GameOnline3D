6900b79da46fe94a87d31809c30baff561fb6828
BuiMinhDat1205 <buiminhdat01022005@gmail.com>
2025-07-21 16:16:42 +0700

    1

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image fillImage;
+    private Transform target;
+
+    public void SetTarget(Transform t)
+    {
+        target = t;
+    }
+
+    public void UpdateHealth(float current, float max)
+    {
+        fillImage.fillAmount = current / max;
+    }
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            transform.position = target.position + Vector3.up * 2f; // điều chỉnh vị trí trên đầu
+            transform.LookAt(Camera.main.transform); // luôn quay về camera
+        }
+    }
+}
