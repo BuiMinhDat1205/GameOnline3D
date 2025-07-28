6ec256d8f3fa1f067074eddb3488fe8ce1860377
BuiMinhDat1205 <buiminhdat01022005@gmail.com>
2025-07-28 16:08:57 +0700

    1
    1

---
+++ Assets/Scripts/ChatSystem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Fusion;
+using TMPro;
+using UnityEngine.UI;
+public class ChatSystem : NetworkBehaviour
+{
+    public TextMeshProUGUI textMessage;
+    public TMP_InputField inputFieldMessage;
+    public Button buttonSend;
+
+    //Chạy ngay sau khi nhân vật được spawn trong network
+    public override void Spawned()
+    {
+        textMessage = GameObject.Find("TextMessage").GetComponent<TextMeshProUGUI>();
+        inputFieldMessage = GameObject.Find("InputFieldMessage").GetComponent<TMP_InputField>();
+        buttonSend = GameObject.Find("ButtonSend").GetComponent<Button>();
+        buttonSend.onClick.AddListener(SendMessageChat);
+    }
+
+    public void SendMessageChat()
+    {
+        var message = inputFieldMessage.text;
+        if (string.IsNullOrWhiteSpace(message)) return;
+        var id = Runner.LocalPlayer.PlayerId;
+        var text = $"Player {id}: {message}";
+        RpcChat(text);
+        inputFieldMessage.text = "";
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    public void RpcChat(string message)
+    {
+        textMessage.text += message + "\n";
+    }
+}
