4bc295a194ace0c0fbdd097667f932cef65eaece
BuiMinhDat1205 <buiminhdat01022005@gmail.com>
2025-08-05 13:04:53 +0700

    1

---
+++ Assets/Scripts/SpawEnemyNetwork.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Fusion;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpawEnemyNetwork : NetworkBehaviour
+
+{
+    [Header("Spawn Enemy")]
+    public Transform[] transformsEnemy;
+    
+    public GameObject[] enemyPrefab;
+    public override void Spawned()
+    {
+       
+        // Chỉ thực hiện khi có quyền sở hữu trạng thái
+        if (Object.HasStateAuthority)
+        {
+           
+            for (int i = 0; i < transformsEnemy.Count(); i++)
+            {
+                var randomIndex = Random.Range(0, enemyPrefab.Length);
+                Runner.Spawn(enemyPrefab[randomIndex], transformsEnemy[i].position, transformsEnemy[i].rotation);
+                   
+               
+               
+            }
+
+        }
+    }
+}
