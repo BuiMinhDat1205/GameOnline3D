94711ed7dff328f7fb57f80bad051b187a07d485
BuiMinhDat1205 <buiminhdat01022005@gmail.com>
2025-08-08 13:40:17 +0700

    1

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -3,22 +3,49 @@ using Fusion;
 
 public class GunScript : NetworkBehaviour
 {
-    public NetworkPrefabRef bulletPrefabs; // Dùng NetworkPrefabRef ?? t??ng thích v?i Runner.Spawn
+    public NetworkPrefabRef bulletPrefabs; 
     public Transform firePoint;
+    public Animator anim; 
+
+    private void Awake()
+    {
+        if (anim == null)
+            anim = GetComponentInParent<Animator>();
+    }
 
     void Update()
     {
         if (Object.HasInputAuthority && Input.GetKeyDown(KeyCode.E))
         {
-            if (Runner.IsServer || Runner.IsSharedModeMasterClient)
-            {
-                Quaternion direction = Quaternion.LookRotation(transform.forward);
-                Quaternion correction = Quaternion.Euler(-90, 180, 0); // Tùy chỉnh theo mô hình bạn
-                Quaternion finalRotation = direction * correction;
-                var bullet = Runner.Spawn(bulletPrefabs, firePoint.position, finalRotation, Object.InputAuthority);
-                bullet.GetComponent<BulletScript>().Owner = Object.InputAuthority;
-                bullet.GetComponent<Rigidbody>().AddForce(firePoint.forward * 40f, ForceMode.Impulse);
-            }
+            RPC_Fire();
         }
+    }
+
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+    public void RPC_Fire()
+    {
+        SpawnBullet();
+        RPC_PlayShootAnim();
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    public void RPC_PlayShootAnim()
+    {
+        if (anim != null)
+        {
+            anim.ResetTrigger("Shoot"); // reset để tránh trigger kẹt
+            anim.SetTrigger("Shoot");
+        }
+    }
+
+    private void SpawnBullet()
+    {
+        Quaternion direction = Quaternion.LookRotation(transform.forward);
+        Quaternion correction = Quaternion.Euler(-90, 180, 0); 
+        Quaternion finalRotation = direction * correction;
+
+        var bullet = Runner.Spawn(bulletPrefabs, firePoint.position, finalRotation, Object.InputAuthority);
+        bullet.GetComponent<BulletScript>().Owner = Object.InputAuthority;
+        bullet.GetComponent<Rigidbody>().AddForce(firePoint.forward * 40f, ForceMode.Impulse);
     }
 }
