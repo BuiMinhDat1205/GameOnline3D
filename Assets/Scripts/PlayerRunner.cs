8610b8e934182da990f4ce0262e191b1f1c277a1
Sticky1705 <111546357+Sticky1705@users.noreply.github.com>
2025-08-11 12:58:24 +0700

    Update

--- Assets/Scripts/PlayerRunner.cs
+++ Assets/Scripts/PlayerRunner.cs
@@ -1,10 +1,12 @@
 ﻿using Fusion;
+using TMPro;
 using UnityEngine;
 
 public class PlayerRunner : SimulationBehaviour, IPlayerJoined
 {
     public GameObject chooserMode;
     public GameObject startGame;
+    public GameObject Taoten;
 
     [Header("Player Setup")]
     public GameObject[] PlayerSpaw; // Danh sách prefab các nhân vật
@@ -15,9 +17,14 @@ public class PlayerRunner : SimulationBehaviour, IPlayerJoined
     public float vY = 1f;
     public float vZ = 0f;
 
+    public TMP_InputField tenNhapvao;
+    public string name;
+
     private void Start()
     {
+        chooserMode.SetActive(true);
         startGame.SetActive(false);
+        Taoten.SetActive(false);
     }
 
     public void PlayerJoined(PlayerRef player)
@@ -27,34 +34,53 @@ public class PlayerRunner : SimulationBehaviour, IPlayerJoined
         if (player == Runner.LocalPlayer)
         {
             Runner.Spawn(
-                PlayerSpaw[chooseChatacter],
-                new Vector3(vX, vY, vZ),
-                Quaternion.identity,
-                player,
-                (runner, obj) =>
-                {
-                    // Sau khi spawn thành công
-                    var playerSetup = obj.GetComponent<PlayerSetUp>();
+            PlayerSpaw[chooseChatacter],
+            new Vector3(vX, vY, vZ),
+            Quaternion.identity,
+            player,
+            (runner, obj) =>
+            {
+                // Sau khi spawn thành công
+                var playerSetup = obj.GetComponent<PlayerSetUp>();
                     if (playerSetup != null)
                     {
                         playerSetup.SetUpCamera(); // Gắn camera follow player
                     }
-                });
 
+                    var createTen = obj.GetComponent<CreateNameNetwork>();
+                    if (createTen != null)
+                    {
+                        createTen.ThemTen(name);
+                    }
+            });
             chooserMode.SetActive(false);
         }
+        else
+        {
+            Debug.LogError($"Invalid character index: {chooseChatacter}. PlayerSpaw length: {PlayerSpaw.Length}");
+        }
+
+
     }
 
     public void ChooseHuman()
     {
         chooseChatacter = 0;
         chooserMode.SetActive(false);
-        startGame.SetActive(true);
+        Taoten.SetActive(true);
     }
     public void ChooseHulk()
     {
+        Debug.Log("Human chosen");
         chooseChatacter = 1;
         chooserMode.SetActive(false);
+        Taoten.SetActive(true);
+    }
+    public void CreateNameCharacter()
+    {
+        Debug.Log($"Name entered: {tenNhapvao.text}");
+        name = tenNhapvao.text;
+        Taoten.SetActive(false);
         startGame.SetActive(true);
     }
 }
